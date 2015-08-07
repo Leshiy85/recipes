578dcb6fe4d6a1d844276b1b79710ae05f530c94
Leshiy85 <Leshiy85.85@mail.ru>
2015-08-07 20:34:49 +0300

    something go wrong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,11 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+		@recipes = @user.recipes.paginate(page: params[:page], per_page: 3)
+	end
+
 	private
 
 		def user_params
